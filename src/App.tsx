2be7396ab05b0019d11c77b84bc653da46d6b041
cold_hailstone <cksn1993@gmail.com>
2023-12-26 16:11:46 +0900

    2.1 Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import Layout from './components/layout';
+import CreateAccount from './routes/create-account';
+import Home from './routes/home';
+import Login from './routes/login';
+import Profile from './routes/profile';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '',
+                element: <Home />,
+            },
+            {
+                path: 'profile',
+                element: <Profile />,
+            },
+        ],
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/create-account',
+        element: <CreateAccount />,
+    },
+]);
+
+const GlobalStyels = createGlobalStyle`
+	${reset};
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		background-color: black;
+		color: white;
+		font-family: 'Courier New', Courier, monospace;
+	}
+`;
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <GlobalStyels />
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
