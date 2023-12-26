907111de3ae2bf5ae34e89c79c7c490c5fac24b8
cold_hailstone <cksn1993@gmail.com>
2023-12-26 17:42:41 +0900

    3.5 Github Login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -10,6 +10,7 @@ import {
     Title,
     Wrapper,
 } from '../components/auth-components';
+import GithubButton from '../components/github-btn';
 import { auth } from '../firebase';
 
 export default function CreateAccount() {
@@ -72,6 +73,7 @@ export default function CreateAccount() {
             <Switcher>
                 Don't have an account? <Link to='/create-account'>Create one &rarr;</Link>
             </Switcher>
+            <GithubButton />
         </Wrapper>
     );
 }
