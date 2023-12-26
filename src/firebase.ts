db5d3f2eaad7d8299547e8234955b13294d74ad9
cold_hailstone <cksn1993@gmail.com>
2023-12-26 16:23:20 +0900

    2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBXZYFPuXbSLsYYw9LkSFgQhl-ZPaDO2f4',
+    authDomain: 'nwitter-ef1f3.firebaseapp.com',
+    projectId: 'nwitter-ef1f3',
+    storageBucket: 'nwitter-ef1f3.appspot.com',
+    messagingSenderId: '1081086931965',
+    appId: '1:1081086931965:web:3307dcb4c12529a7649f94',
+};
+
+const app = initializeApp(firebaseConfig);
